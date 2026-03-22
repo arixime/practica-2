800b5a8106e85966f4d5b138b22a493f12a88495
arixime <aimlordasukaro@gmail.com>
2026-03-22 22:48:47 +0300

    Add files via upload

---
+++ Scr/Примеры C#/WebAPI/Models/Image.cs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebAPI.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public int Width {  get; set; }
+        public int Height { get; set; }
+        public string Type { get; set; }
+        public DateTime AddedDate { get; set; }
+        public string FilePath { get; set; }
+    }
+}
